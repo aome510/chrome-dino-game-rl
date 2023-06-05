1b764e4bb1dc1fd7db51992f845ed601c12c3c1b
Thang Pham <phamducthang1234@gmail.com>
2023-06-04 23:51:49 -0400

    implemented render function for running track

--- envs.py
+++ envs.py
@@ -1,4 +1,5 @@
 import enum
+import os
 from typing import Any, TypedDict
 import gymnasium as gym
 import numpy as np
@@ -20,16 +21,30 @@ class RenderMode(str, enum.Enum):
     RGB = "rgb_array"
 
 
+class Assets:
+    def __init__(self):
+        self.track = pygame.image.load(os.path.join("assets", "Track.png"))
+
+
 class Env(gym.Env):
     metadata = {"render_fps": 10, "render_modes": [RenderMode.HUMAN, RenderMode.RGB]}
 
     def __init__(
         self,
         render_mode: RenderMode | None,
-        window_size: tuple[int, int] = (256, 256),
+        window_size: tuple[int, int] = (1024, 512),
     ) -> None:
+        self.assets = Assets()
+
         self.render_mode = render_mode
         self.window_size = window_size
+
+        self.score = 0
+        self.speed = 20
+
+        self.track_offset_x = 0
+        self.track_w = self.assets.track.get_width()
+        self.track_h = self.assets.track.get_height()
 
         self.action_space = gym.spaces.Discrete(2)
         self.observation_space = gym.spaces.Dict(
@@ -72,6 +87,12 @@ class Env(gym.Env):
         obs = self._get_obs()
         info = self._get_info()
 
+        self.score += 1
+        if self.score % 100 == 0:
+            self.speed += 1
+        # Negative offset to slide the running track image to the left
+        self.track_offset_x -= self.speed
+
         if self.render_mode == RenderMode.HUMAN:
             self._render_frame()
 
@@ -81,9 +102,35 @@ class Env(gym.Env):
         if self.render_mode == RenderMode.RGB:
             return self._render_frame()
 
+    def _render_track(self, canvas: pygame.Surface):
+        # Render the running track image moved to the left by `track_offset_x`
+        canvas.blit(
+            self.assets.track,
+            (self.track_offset_x, self.window_size[1] - self.track_h),
+        )
+
+        # If the moved image doesn't cover the screen, render the left space
+        # with a second image to create a "loop" effect.
+        if self.track_offset_x + self.track_w < self.window_size[0]:
+            # Find the starting position to render the second image
+            # -10 here because the running track image starts with a small gap
+            start_x = self.track_offset_x + self.track_w - 10
+            canvas.blit(
+                self.assets.track,
+                (start_x, self.window_size[1] - self.track_h),
+            )
+
+            # If the starting position is negative, which means the moved image
+            # doesn't intersect with the screen, start rendering a new image with
+            # a new offset equal to the starting position
+            if start_x <= 0:
+                self.track_offset_x = start_x
+
     def _render_frame(self):
         canvas = pygame.Surface(self.window_size)
         canvas.fill((255, 255, 255))
+
+        self._render_track(canvas)
 
         if self.window is not None and self.clock is not None:
             self.window.blit(canvas, canvas.get_rect())
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import envs
 env = gym.make("Env-v0", render_mode="human")
 env.reset()
 
-for _ in range(10):
+for _ in range(1000):
     env.step(envs.Action.UP)
 
 env.close()
