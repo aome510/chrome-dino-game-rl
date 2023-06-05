1c012da23f432fdf1969482a517f832afa66bbd4
Thang Pham <phamducthang1234@gmail.com>
2023-06-05 11:19:56 -0400

    decouple track render code from `Env` class

--- envs.py
+++ envs.py
@@ -25,6 +25,54 @@ class Assets:
     def __init__(self):
         self.track = pygame.image.load(os.path.join("assets", "Track.png"))
 
+        # dino assets
+        self.dino_run1 = pygame.image.load(os.path.join("assets", "DinoRun1.png"))
+        self.dino_run2 = pygame.image.load(os.path.join("assets", "DinoRun2.png"))
+
+
+class Dino:
+    def __init__(self):
+        ...
+
+    def render(self, canvas: pygame.Surface):
+        ...
+
+
+class Track:
+    def __init__(self, assets: Assets):
+        self.assets = assets
+        self.track_offset_x = 0
+        self.track_w = self.assets.track.get_width()
+        self.track_h = self.assets.track.get_height()
+
+    def step(self, speed: int):
+        # Negative offset to slide the running track image to the left
+        self.track_offset_x -= speed
+
+    def render(self, canvas: pygame.Surface, window_size: tuple[int, int]):
+        # Render the running track image moved to the left by `track_offset_x`
+        canvas.blit(
+            self.assets.track,
+            (self.track_offset_x, window_size[1] - self.track_h),
+        )
+
+        # If the moved image doesn't cover the screen, render the left space
+        # with a second image to create a "loop" effect.
+        if self.track_offset_x + self.track_w < window_size[0]:
+            # Find the starting position to render the second image
+            # -10 here because the running track image starts with a small gap
+            start_x = self.track_offset_x + self.track_w - 10
+            canvas.blit(
+                self.assets.track,
+                (start_x, window_size[1] - self.track_h),
+            )
+
+            # If the starting position is negative, which means the moved image
+            # doesn't intersect with the screen, start rendering a new image with
+            # a new offset equal to the starting position
+            if start_x <= 0:
+                self.track_offset_x = start_x
+
 
 class Env(gym.Env):
     metadata = {"render_fps": 10, "render_modes": [RenderMode.HUMAN, RenderMode.RGB]}
@@ -34,17 +82,8 @@ class Env(gym.Env):
         render_mode: RenderMode | None,
         window_size: tuple[int, int] = (1024, 512),
     ) -> None:
-        self.assets = Assets()
-
+        # Initialize `gym.Env` required fields
         self.render_mode = render_mode
-        self.window_size = window_size
-
-        self.score = 0
-        self.speed = 20
-
-        self.track_offset_x = 0
-        self.track_w = self.assets.track.get_width()
-        self.track_h = self.assets.track.get_height()
 
         self.action_space = gym.spaces.Discrete(2)
         self.observation_space = gym.spaces.Dict(
@@ -53,15 +92,26 @@ class Env(gym.Env):
             }
         )
 
-        self.window = None
-        self.clock = None
+        # Other (private) fields
+        self._assets = Assets()
+        self._window_size = window_size
+
+        self._score = 0
+        self._speed = 20
+
+        # Initialize environment's objects' states
+        self._track = Track(self._assets)
+
+        # Initialize `pygame` data
+        self._window = None
+        self._clock = None
 
         if self.render_mode == RenderMode.HUMAN:
             pygame.init()
             pygame.display.init()
 
-            self.window = pygame.display.set_mode(self.window_size)
-            self.clock = pygame.time.Clock()
+            self._window = pygame.display.set_mode(self._window_size)
+            self._clock = pygame.time.Clock()
 
         super().__init__()
 
@@ -87,11 +137,11 @@ class Env(gym.Env):
         obs = self._get_obs()
         info = self._get_info()
 
-        self.score += 1
-        if self.score % 100 == 0:
-            self.speed += 1
-        # Negative offset to slide the running track image to the left
-        self.track_offset_x -= self.speed
+        self._score += 1
+        if self._score % 100 == 0:
+            self._speed += 1
+
+        self._track.step(self._speed)
 
         if self.render_mode == RenderMode.HUMAN:
             self._render_frame()
@@ -102,45 +152,27 @@ class Env(gym.Env):
         if self.render_mode == RenderMode.RGB:
             return self._render_frame()
 
-    def _render_track(self, canvas: pygame.Surface):
-        # Render the running track image moved to the left by `track_offset_x`
-        canvas.blit(
-            self.assets.track,
-            (self.track_offset_x, self.window_size[1] - self.track_h),
-        )
-
-        # If the moved image doesn't cover the screen, render the left space
-        # with a second image to create a "loop" effect.
-        if self.track_offset_x + self.track_w < self.window_size[0]:
-            # Find the starting position to render the second image
-            # -10 here because the running track image starts with a small gap
-            start_x = self.track_offset_x + self.track_w - 10
-            canvas.blit(
-                self.assets.track,
-                (start_x, self.window_size[1] - self.track_h),
-            )
-
-            # If the starting position is negative, which means the moved image
-            # doesn't intersect with the screen, start rendering a new image with
-            # a new offset equal to the starting position
-            if start_x <= 0:
-                self.track_offset_x = start_x
-
     def _render_frame(self):
-        canvas = pygame.Surface(self.window_size)
+        canvas = pygame.Surface(self._window_size)
         canvas.fill((255, 255, 255))
 
-        self._render_track(canvas)
+        self._track.render(canvas, self._window_size)
 
-        if self.window is not None and self.clock is not None:
-            self.window.blit(canvas, canvas.get_rect())
+        if self._window is not None and self._clock is not None:
+            self._window.blit(canvas, canvas.get_rect())
+
             pygame.event.pump()
             pygame.display.update()
 
-            self.clock.tick(self.metadata["render_fps"])
+            self._clock.tick(self.metadata["render_fps"])
+        else:
+            # return the canvas as a rgb array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
 
     def close(self):
-        if self.window is not None:
+        if self._window is not None:
             pygame.display.quit()
             pygame.quit()
 
